01f75feca6f9f8a426fda884867f52a11cf30516
vanja-cvetkovic-ey <Vanja.Cvetkovic@rs.ey.com>
2022-07-05 12:00:57 +0200

    control structures

---
+++ basics/controlStructures.js
@@ -0,0 +1,27 @@
+// if statments
+
+var condition = 2;
+var anotherCondition = true;
+
+if (condition) {
+  console.log("executed");
+} else if (anotherCondition) {
+  console.log("still executed");
+} else {
+  console.log("not executed");
+}
+
+console.log(2 == true);
+
+// switch
+
+var luckyNumber = 7;
+
+switch (luckyNumber) {
+  case 1:
+    console.log("is 1");
+  case 8:
+    console.log("is 8");
+  default:
+    console.log("default");
+}
