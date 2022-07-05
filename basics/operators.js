a89c1fb58b81eed9fd741c80e0f04c7d6d4bf18f
vanja-cvetkovic-ey <Vanja.Cvetkovic@rs.ey.com>
2022-07-05 12:47:56 +0200

    operators1

--- basics/operators.js
+++ basics/operators.js
@@ -1,6 +1,19 @@
-var a = 3;
-var b = NaN;
-var c = true;
+// var a = 3;
+// var b = NaN;
+// var c = true;
 
-console.log(a + b);
-console.log(a + c);
+// console.log(a + b);
+// console.log(a + c);
+
+// substraction
+
+// var a1 = "12";
+// var b1 = "1";
+
+// console.log(a1 - b);
+
+// multification & floating point
+var a = 2.2;
+var b = 3.3;
+
+console.log(a * b);
