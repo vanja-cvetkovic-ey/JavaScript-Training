f81b82f53e063c2c3d8a5974af3d244b5cf2514c
vanja-cvetkovic-ey <Vanja.Cvetkovic@rs.ey.com>
2022-07-05 13:57:55 +0200

    operators

--- basics/operators.js
+++ basics/operators.js
@@ -13,7 +13,28 @@
 // console.log(a1 - b);
 
 // multification & floating point
-var a = 2.2;
-var b = 3.3;
+// var a = 2.2;
+// var b = 3.3;
 
-console.log(a * b);
+// console.log(a * b);
+
+// problem with floating point & toFixed()
+// var a = 1.3;
+// var b = 2.2;
+
+// console.log(a * b);
+
+// if ((a * b).toFixed() == 2.86) {
+//   console.log(true);
+// } else {
+//   console.log("false");
+// }
+
+// division & modulus
+
+// var a = 3.3;
+// var b = 2.2;
+// // problem with floating point
+// console.log((a / b).toFixed(2));
+
+// modules %
