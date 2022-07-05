b8e21ca0d84dd56df93862d713669db2f9822f83
vanja-cvetkovic-ey <Vanja.Cvetkovic@rs.ey.com>
2022-07-05 17:00:54 +0200

    this

--- objects/objects.js
+++ objects/objects.js
@@ -23,3 +23,30 @@ max.greet();
 ana.greet();
 
 console.log(ana.__proto__ == person);
+
+// with new keyword we are "constructing" a new object using the constructor function
+// javascript automaticly gives us a prototype for each of our functions here or of our objects created of this constructor function
+
+function Person() {}
+
+var person = new Person();
+person.name = "Max";
+
+console.log(person.__proto__ == Person.prototype);
+
+// so that is a certain prototype automatically created for us here which we can use
+
+Person.prototype.greet = function () {
+  console.log("hello");
+};
+
+// constructor functions let us creates objects with some default fields and values if you want to put like it this
+// prototypes let allow us to inherite from objects, so ey are on different levels
+
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+var max = new Person("max", 27);
+var ana = new Person("ana", 32);
