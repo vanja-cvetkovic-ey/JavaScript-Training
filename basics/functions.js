8ed28f3ce5ec161822e2079be0f462ed8dfc94a5
vanja-cvetkovic-ey <Vanja.Cvetkovic@rs.ey.com>
2022-07-05 11:48:19 +0200

    functions

---
+++ basics/functions.js
@@ -0,0 +1,6 @@
+function calc() {
+  console.log("inside function");
+}
+
+var anotherFn = calc();
+console.log(anotherFn);
--- basics/variables.js
+++ basics/variables.js
@@ -1,4 +1,10 @@
+"use strict";
+
 // var var1 = "text";
 // var var1=NaN;
 // var var1 = undefiend;
+
+var var1 = {
+  name: "Max",
+};
 console.log(typeof var1);
